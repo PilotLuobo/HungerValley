4f366c83c83a513047290eacc62dee9b4c241b5e
PilotLuobo <97351832@qq.com>
2018-12-22 00:55:21 +0800

    苹果风格轮播图。完成！！

--- Apple-like Gallery Slideshow/main.js
+++ Apple-like Gallery Slideshow/main.js
@@ -1,5 +1,41 @@
-$('.opt1').addClass('active');
+$('a.opt1').addClass('active');
 
-$('.opt2').on(click, function (e) {
-    e.addClass('active');
+$('a.opt1').on('click', function () {
+    $('a.opt1').addClass('active');
+    $('a.opt2').removeClass('active');
+    $('a.opt3').removeClass('active');
+    $('a.opt4').removeClass('active');
+    $('.images').css({
+        transform: 'translate(0)'  
+    })
+});
+
+$('a.opt2').on('click', function () {
+    $('a.opt1').removeClass('active');
+    $('a.opt2').addClass('active');
+    $('a.opt3').removeClass('active');
+    $('a.opt4').removeClass('active');
+    $('.images').css({
+        transform: 'translate(-100%)'
+    })
+});
+
+$('a.opt3').on('click', function () {
+    $('a.opt1').removeClass('active');
+    $('a.opt2').removeClass('active');
+    $('a.opt3').addClass('active');
+    $('a.opt4').removeClass('active');
+    $('.images').css({
+        transform: 'translate(-200%)'
+    })
+});
+
+$('a.opt4').on('click', function () {
+    $('a.opt1').removeClass('active');
+    $('a.opt2').removeClass('active');
+    $('a.opt3').removeClass('active');
+    $('a.opt4').addClass('active');
+    $('.images').css({
+        transform: 'translate(-300%)'
+    })
 });
